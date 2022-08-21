86704b22ed61a1720dc045031ab667d4d19f6d5f
Aline Santos <enila404@gmail.com>
2022-08-20 21:49:29 -0300

    Offer added

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -17,4 +17,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 -- converte o camel case pra snake case
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Html', 'https://via.placeholder.com/300/008000', 'https://via.placeholder.com/300')
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Html', 'https://via.placeholder.com/300/008000', 'https://via.placeholder.com/300');
+
+--instant usar o mesmo padrão que vale para o h2, postgrees -> ISO8601
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00',1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00',1);
