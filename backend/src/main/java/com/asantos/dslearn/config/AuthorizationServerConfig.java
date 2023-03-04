826df9bd4568980b9782440225dad51e1ae8d8f0
Aline Santos <enila404@gmail.com>
2023-03-04 16:31:59 -0300

    feat: refresh token

--- backend/src/main/java/com/asantos/dslearn/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/asantos/dslearn/config/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -45,6 +46,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private JwtTokenEnhancer tokenEnhancer;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -53,8 +57,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret))
-				.scopes("read", "write").authorizedGrantTypes("password").accessTokenValiditySeconds(jwtDuration);
+		clients.inMemory()
+				.withClient(clientId)
+				.secret(passwordEncoder.encode(clientSecret))
+				.scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token") //um tipo de grant types
+				.accessTokenValiditySeconds(jwtDuration)
+				.refreshTokenValiditySeconds(jwtDuration); //terá o mesmo tempo de expiração do jwt token
 	}
 
 	@Override
@@ -65,7 +74,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		
 		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore)
 				.accessTokenConverter(accessTokenConverter)
-				.tokenEnhancer(chain);
+				.tokenEnhancer(chain)
+				.userDetailsService(userDetailsService);
 	}
 
 }
